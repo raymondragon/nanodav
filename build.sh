01cb2b840f1d30a99e695c90897bd8c28134fe9b
raymondragon <54016243+raymondragon@users.noreply.github.com>
2023-03-07 17:23:35 +0800

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,10 @@
+nohup env CGO_ENABLED=0 GOOS=linux GOARCH=386 go build -o nanodav-linux-386 &
+nohup env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o nanodav-linux-amd64 &
+nohup env CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -o nanodav-linux-arm64 &
+nohup env CGO_ENABLED=0 GOOS=windows GOARCH=386 go build -o nanodav-windows-386.exe &
+nohup env CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o nanodav-windows-amd64.exe &
+nohup env CGO_ENABLED=0 GOOS=windows GOARCH=arm64 go build -o nanodav-windows-arm64.exe &
+nohup env CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o nanodav-darwin-amd64 &
+nohup env CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -o nanodav-darwin-arm64 &
+nohup env CGO_ENABLED=0 GOOS=freebsd GOARCH=amd64 go build -o nanodav-freebsd-amd64 &
+nohup env CGO_ENABLED=0 GOOS=freebsd GOARCH=arm64 go build -o nanodav-freebsd-arm64 &
